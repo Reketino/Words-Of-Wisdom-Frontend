05e9f7990c3b13955705bd9d7e7ae18d734e0406
Reketino <bjornevensk8@gmail.com>
2025-12-02 20:17:39 +0100

    cleaning me files

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,8 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import MusicPlayer from "../components/musikk";
-import Translator from "../components/Oversetter";
+import MusicPlayer from "@/components/musikk";
+import Translator from "@/components/Oversetter";
 
 
 export default function Home() {
